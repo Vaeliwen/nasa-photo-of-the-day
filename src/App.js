69da510bdc73e0a8a1c9612a168f3876b4d2ebd1
Vaeliwen <zaman@imbriaco.com>
2019-07-10 16:19:21 -0400

    Added basic fetch w/ API key.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState()
+
+  const fetchSpace = () => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=5b0JU9caOsvt0R1kREtfXxgEyWnGhPCwWaINsFtt&date=2012-03-14`)
+      .then(res => res.json())
+      .then(res => console.log(res.hdurl))
+      .catch(err => console.log('Houston, we have a problem.'))
+  }
+
+
+  useEffect(() => {
+    fetchSpace()
+  }, [])
+
   return (
     <div className="App">
       <p>
